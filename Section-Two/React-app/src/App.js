5333c2202c23432d2e78978b944ff80b7d611f64
Jonathan Chiang <jonathanchiang7@gmail.com>
2021-12-12 17:10:10 -0500

    Section3-PartB-Change complete

---
+++ Section-Two/React-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Change from './Change';
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      < Change />
+    </div>
+  );
+}
+
+export default App;
