f99980aecf39743d78cbcb0105e0ab33a97762b6
Jonathan Chiang <jonathanchiang7@gmail.com>
2021-12-12 13:54:50 -0500

    section3-part a finished

--- server.js
+++ server.js
@@ -5,85 +5,67 @@ app.use(express.json());
 
 const PORT = 3000;
 
-
-var todos = [
-  { id: uuidv4(), name: "Jon", task: "buy the milk" },
-  { id: uuidv4(), name: "May", task: "eat the bread" },
-  { id: uuidv4(), name: "Gary", task: "rent a car" },
-  { id: uuidv4(), name: "Michael", task: "walk the dog" },
-  { id: uuidv4(), name: "Ken", task: "go to work" },
+var recycledItems = [
+  { Name: "Newspaper", Description: "paper", Recyclable: true, Quantity: 10, PPU: 1, id: uuidv4() },
+  { Name: "CardBoard", Description: "4x4 cardboard box", Recyclable: true, Quantity: 12, PPU: 10, id: uuidv4() },
+  { Name: "Plastic Bottle", Description: "8oz Dasani Water Bottle", Recyclable: true, Quantity: 54, PPU: 1, id: uuidv4() },
+  { Name: "Aluminum Can", Description: "Campbells Soup ", Recyclable: true, Quantity: 25, PPU: 5, id: uuidv4() },
+  { Name: "iPhone", Description: "electronic device", Recyclable: false, Quantity: 73, PPU: 900, id: uuidv4() },
+  { Name: "Plastic Bag", Description: "Wegman's plastic bag", Recyclable: false, Quantity:754, PPU: 3, id: uuidv4() },
 ];
 
-//display entire object on home page
-app.get("/task", function (req, res, next) {
-  res.status(200).send(todos);
+//display entire object - works!
+app.get("/recycle",(req, res, next) => {
+  res.status(200).send(recycledItems);
 });
 
-//find single task
-//req.params will be what endpoint postman will be looking for. They must match
-app.get("/task/:id", (req, res, next) => {
-    let { id } = req.params;    //returns {id: 1} or whatever number you input
-    const findTask = todos.filter((task) => task.id === id);    //filter todo array by id 
-    res.status(200).send(findTask);
+//find single object by id - works!
+app.get("/recycle/:id", (req, res, next) => {
+    let { id } = req.params;
+    const findItem = recycledItems.filter((item) => item.id === id);   
+    res.status(200).send(findItem);
 })
 
-//create new task and return entire array with new task
-//req.body: allows you to access data in a string/JSON object from client side
-app.post("/task", function (req, res) {
-  var newTask = req.body;   //create newTask variable as a new req.body object. Follows the same formatting as current todo object
-  console.log("REQ.BODY",newTask);  //Returns: { id: '10', name: 'Jason', task: 'drink water and coffee' }
-  todos.push({ id: uuidv4(), ...newTask }); //push new task with unique id while keeping original array
+//create new item and return the updated array with new item - works!
+app.post("/recycle", (req, res) => {
+  var newItem = req.body;  
+  console.log("REQ.BODY",newItem);
+  recycledItems.push({...newItem, id: uuidv4() });
   res.status(200)
-//   .send(`Task: ${newTask.task} has been added`)
-  .redirect("/task");
+  .redirect("/recycle");
 });
 
-//When creating new object entry in postman, leave id out since you're pushing a new one in
-// {
-//     "name": "tootsie",
-//     "task": "poop"
-// }
-
-
-//update task and name on single todo item
-app.put("/task/:id", (req, res, next) => {
-    let { id } = req.params;    //returns {id: 1}
-    const { name, task } = req.body;    //Returns { name: 'Jon', task: 'yur DEEZ' }
-    const findTask = todos.find((task) => task.id === id);
-    if (id === undefined){                          //if id doesn't exist, create error and pass it along to next function 
+//update name and description on single item - works!
+app.put("/recycle/:id", (req, res, next) => {
+    let { id } = req.params;
+    const { Name, Description } = req.body;
+    const findItem = recycledItems.find((item) => item.id === id);
+    if (id === undefined){
         const err = new Error("Task doesn't exist")
         next(err);
     }
-    if (name) findTask.name = name; //if name exist, current name = new name 
-    if (task) findTask.task = task; //if task exist, current task = new task
+    if (Name) findItem.Name = Name;
+    if (Description) findItem.Description = Description;
 
-    res.status(200)
-    // .send(`Task: ${id} has been updated`)
-    .redirect("/task")
+    res.status(200).redirect("/recycle")
 })
 
-// delete single task by id
-app.delete("/task/:id", (req, res, next) => {
-    let { id } = req.params;    //returns { id: '1' }
-    todos = todos.filter((task) => task.id !== id); //filter todo list by elements that are not the selected id
-    res.status(200).send(todos);
+// delete single task by id - works!
+app.delete("/recycle/:id", (req, res, next) => {
+    let { id } = req.params;
+    recycledItems = recycledItems.filter((item) => item.id !== id);
+    res.status(200).send(recycledItems);
 })
 
-//delete single task by name -> only one delete method can be used at a time, please comment out one before testing 
-app.delete("/task/:name", (req, res, next) => {
-    let { name } = req.params;  //returns { name: 'May' }
-    todos = todos.filter((task) => task.name !== name);
-    res.status(200).send(todos);
-})
-
+//if error occurs, create error object
 app.use((req, res, next) => {
-    const err = new Error("Task not found");    
+    const err = new Error("Task not found");
     err.status = 404;
     next(err);
 })
 
 //global error handling function 
-app.use((err, req, res, next) => {  //if error occurs, display this object 
+app.use((err, req, res, next) => {  //if error occurs, display this object with status and message
     res.status(err.status || 500)
     res.send({
         error: {
@@ -96,8 +78,3 @@ app.use((err, req, res, next) => {  //if error occurs, display this object
 app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`);
 });
-
-// GET / retrieve the list of tasks
-// POST / create a new task
-// PUT /:id update the task of id
-// DELETE /:id
