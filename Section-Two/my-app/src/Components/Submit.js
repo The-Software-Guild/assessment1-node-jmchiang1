e52c34ce1b875b1df70d4455bdc8445bc17c5356
Jonathan Chiang <jonathanchiang7@gmail.com>
2021-12-13 01:00:06 -0500

    section3-partB-submit: stuck on proper state management

--- Section-Two/my-app/src/Components/Submit.js
+++ Section-Two/my-app/src/Components/Submit.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Submit.css";
 
 function Submit() {
-    return (
+  const [name, setName] = useState([]);
+  const [age, setAge] = useState("");
+
+  const handleName = (e) => {
+    //   setName(e.target.value);
+    setName(name.concat(e.target.value));
+  };
+
+  const handleAge = (e) => {
+    setAge(e.target.value);
+  };
+
+  return (
+    <div className="submit">
+      <form>
+        <input
+          id="name"
+          placeholder="Name..."
+          type="text"
+          onChange={handleName}
+        />
+        <input
+          id="age"
+          placeholder="Age..."
+          type="number"
+          onChange={handleAge}
+          value={age}
+        />
+        <button type="button" value="submit">
+          Click Me
+        </button>
         <div>
-            <h1>Submit</h1>
+          <ul>
+            {name.map((user) => (
+              <div key={user.id}>
+                <p>{user}</p>
+              </div>
+            ))}
+          </ul>
         </div>
-    )
+        {/* <h1>Age: {age}</h1> */}
+      </form>
+    </div>
+  );
 }
 
-export default Submit
+export default Submit;
+
+//   setNames(names => [...names, newName])
