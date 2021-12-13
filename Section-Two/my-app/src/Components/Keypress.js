9c2f6d0ad57bf490ad27227fa6ff535812d89b50
Jonathan Chiang <jonathanchiang7@gmail.com>
2021-12-12 21:41:07 -0500

    section3-partA-keypress is complete

--- Section-Two/my-app/src/Components/Keypress.js
+++ Section-Two/my-app/src/Components/Keypress.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Keypress() {
-    return (
-        <div>
-            <h1>Kepyress</h1>
-        </div>
-    )
+  const [key, setKey] = useState("");
+  const handleChange = (event) => {
+    setKey(event.keyCode);
+  };
+
+  return (
+    <div>
+      <h1>Key Code Translator</h1>
+      <h3>Press any key, and see what it's key code is</h3>
+      <input type="text" onKeyDown={(e) => handleChange(e)}></input>
+      <div>Key pressed is {key} </div>
+    </div>
+  );
 }
 
-export default Keypress
+export default Keypress;
