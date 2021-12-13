42e713218ea0f202dfd1d5b12607b3781f2c6793
Jonathan Chiang <jonathanchiang7@gmail.com>
2021-12-13 10:37:52 -0500

    section3-partB-submit complete

--- Section-Two/my-app/src/Components/Submit.js
+++ Section-Two/my-app/src/Components/Submit.js
@@ -2,52 +2,40 @@ import React, { useState } from "react";
 import "./Submit.css";
 
 function Submit() {
-  const [name, setName] = useState([]);
-  const [age, setAge] = useState("");
-
-  const handleName = (e) => {
-    //   setName(e.target.value);
-    setName(name.concat(e.target.value));
-  };
-
-  const handleAge = (e) => {
-    setAge(e.target.value);
-  };
-
-  return (
-    <div className="submit">
-      <form>
-        <input
-          id="name"
-          placeholder="Name..."
-          type="text"
-          onChange={handleName}
-        />
-        <input
-          id="age"
-          placeholder="Age..."
-          type="number"
-          onChange={handleAge}
-          value={age}
-        />
-        <button type="button" value="submit">
-          Click Me
-        </button>
-        <div>
-          <ul>
-            {name.map((user) => (
-              <div key={user.id}>
-                <p>{user}</p>
-              </div>
-            ))}
-          </ul>
-        </div>
-        {/* <h1>Age: {age}</h1> */}
-      </form>
-    </div>
-  );
-}
+    const [item, setItem] = useState([]); //state of array
+    const [name, setName] = useState(""); //state of name input
+    const [age, setAge] = useState(""); //state of age input
+  
+    const saveName = (e) => {
+      //save input value (event.target.value)
+      setName(e.target.value);
+    };
+  
+    const saveAge = (e) => {
+      setAge(e.target.value);
+    };
+  
+    const addNewItem = () => {
+      //push input value into array so it stays
+      const arr = [...item];
+      arr.push(`Name: ${name}, Age: ${age}`); //push this into array
+      setItem(arr);
+      setAge(arr);
+      setName("");
+      setAge("");
+    };
+    return (
+      <div className="submit">
+        <input type="text" onChange={saveName} placeholder="Name..." />
+        <input type="number" onChange={saveAge} placeholder="Age..." />
+        <button onClick={addNewItem}>Submit</button>
+        <ol>
+          {item.map((item, i) => {
+            return <li key={i}>{item}</li>;
+          })}
+        </ol>
+      </div>
+    );
+  }
 
 export default Submit;
-
-//   setNames(names => [...names, newName])
